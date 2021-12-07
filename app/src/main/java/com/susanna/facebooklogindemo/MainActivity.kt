8b980f297e3a7dafdc0a8cc54ae1ba4dbed856e8
susannasameh <you@domain.com>
2021-12-07 21:55:06 +0200

    first commit

---
+++ app/src/main/java/com/susanna/facebooklogindemo/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.susanna.facebooklogindemo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    var callback: CallbackManager? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+       callback= CallbackManager.Factory.create()
+        loginButton.registerCallback(callback, object :FacebookCallback<LoginResult>{
+            override fun onCancel() {
+                TODO("Not yet implemented")
+                textView.text =" login canceled"
+            }
+
+            override fun onError(error: FacebookException) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onSuccess(result: LoginResult) {
+                TODO("Not yet implemented")
+                textView.text= "Login Success${result?.accessToken?.userId}"+
+                        "${result?.accessToken?.token}"
+            }
+
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callback?.onActivityResult(requestCode,resultCode,data)
+
+    }
+}
